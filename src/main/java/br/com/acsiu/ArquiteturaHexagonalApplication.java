6c10e4d4fa720fc7fd98e5381da7ae4579176922
Helio Alves <acsiut@gmail.com>
2023-09-18 21:22:05 -0300

    Configurado debug com gradle

--- src/main/java/br/com/acsiu/ArquiteturaHexagonalApplication.java
+++ src/main/java/br/com/acsiu/ArquiteturaHexagonalApplication.java
@@ -1,17 +1,16 @@
 package br.com.acsiu;
 
+import br.com.acsiu.infraestrutura.adaptadores.interfaces.IColaboradorRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import br.com.acsiu.infraestrutura.adaptadores.interfaces.IColaboradorRepository;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = IColaboradorRepository.class)
 public class ArquiteturaHexagonalApplication {
 
     public static void main(String[] args) {
-        // System.setProperty("spring.devtools.restart.enabled", "true");
+        System.setProperty("spring.devtools.restart.enabled", "true");
         SpringApplication.run(ArquiteturaHexagonalApplication.class, args);
     }
 }
